01a5df1de6a9e189ee967029ec318c05458a26cf
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-22 12:06:07 -0600

    feat: Add theme toggle button component with sun and moon icons

---
+++ resources/views/components/toggle-theme.blade.php
@@ -0,0 +1,13 @@
+  <div class="fixed bottom-0 right-0 m-3">
+      <button
+          class="flex items-center justify-center gap-2 rounded-lg border border-zinc-200 p-2 font-medium text-zinc-600 transition-colors duration-300 hover:bg-zinc-100 dark:border-zinc-900 dark:text-white dark:hover:bg-zinc-900"
+          id="themeButton">
+          <span id="icon-sun">
+              <x-icon icon="sun" class="h-5 w-5" />
+          </span>
+
+          <span id="icon-moon" class="hidden">
+              <x-icon icon="moon" class="h-5 w-5" />
+          </span>
+      </button>
+  </div>
