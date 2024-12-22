c27d50acc1b36c34b9053cc4a575cbddd52637ae
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-22 12:04:12 -0600

    feat: Implement scholarship assignment and unassignment functionality with search

---
+++ resources/js/proyectos.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    $(".assign-button").click(function () {
+        const scholarItem = $(this).closest(".scholar-item");
+        scholarItem.fadeOut(300, function () {
+            scholarItem.find(".assign-button").addClass("hidden");
+            scholarItem.find(".unassign-button").removeClass("hidden");
+            $("#assigned-scholars").append(scholarItem);
+            scholarItem.fadeIn(300);
+            $("#assigned-scholars .no-assigned").remove();
+        });
+        $("#search").val("");
+        $("#scholar-list .scholar-item").show();
+    });
+
+    $(".unassign-button").click(function () {
+        const scholarItem = $(this).closest(".scholar-item");
+        scholarItem.fadeOut(300, function () {
+            scholarItem.find(".unassign-button").addClass("hidden");
+            scholarItem.find(".assign-button").removeClass("hidden");
+
+            $("#scholar-list").append(scholarItem);
+            scholarItem.fadeIn(300);
+            if (
+                $("#assigned-scholars").children(".scholar-item").length === 0
+            ) {
+                $("#assigned-scholars").append(`
+                <div class="flex h-32 items-center justify-center no-assigned">
+                    <p class="text-sm dark:text-zinc-200">
+                        No hay becados asignados
+                    </p>
+                </div>
+            `);
+            }
+        });
+    });
+
+    $("#search").on("keyup", function () {
+        const value = $(this).val().toLowerCase();
+        $("#scholar-list .scholar-item").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+        });
+    });
+
+    $("#assign-button").on("click", function () {
+        const ids = [];
+        $("#scholarship-inputs").empty();
+        $("#assigned-scholars .scholar-item").each(function () {
+            const scholarId = $(this).data("id");
+            if (scholarId) {
+                ids.push(scholarId);
+                $("#scholarship-inputs").append(`
+                <input type="hidden" name="scholarship_id[]" value="${scholarId}">
+            `);
+            }
+        });
+        $("#assign-form").submit();
+    });
+});
