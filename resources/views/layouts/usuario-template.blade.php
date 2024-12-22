60da69e5489ac9e6e745f81203c0c1af4d81ca72
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-22 12:06:19 -0600

    feat: Add user template layout with theme detection and essential scripts

---
+++ resources/views/layouts/usuario-template.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        (function() {
+            let theme = localStorage.getItem('theme') || "light"
+            if (theme === 'dark' || (!('theme' in localStorage) && window.matchMedia(
+                        '(prefers-color-scheme: dark)')
+                    .matches)) {
+                document.documentElement.classList.add('dark');
+
+            } else {
+                document.documentElement.classList.add('light');
+            }
+        })();
+    </script>
+</head>
+
+<body class="mx-auto flex w-[60%] flex-col overflow-x-hidden bg-white dark:bg-black">
+    @include('layouts.__partials.usuario.navbar')
+    <main>
+        @include('layouts.__partials.alert')
+        @yield('content')
+    </main>
+</body>
+@vite('resources/js/app.js')
+
+
+</html>
