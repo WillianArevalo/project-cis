cd656b903a15cc80c572bb9661eac96947adc948
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-22 12:06:13 -0600

    feat: Add admin template layout with theme toggle and essential scripts

---
+++ resources/views/layouts/admin-template.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
+    <script>
+        (function() {
+            let theme = localStorage.getItem('theme') || "light"
+            if (theme === 'dark' || (!('theme' in localStorage) && window.matchMedia(
+                        '(prefers-color-scheme: dark)')
+                    .matches)) {
+                document.documentElement.classList.add('dark');
+
+            } else {
+                document.documentElement.classList.add('light');
+            }
+        })();
+    </script>
+</head>
+
+<body class="flex flex-col overflow-x-hidden bg-white dark:bg-black">
+    @include('layouts.__partials.admin.navbar')
+    <main class="h-full w-full bg-white dark:bg-black sm:ps-64">
+        @include('layouts.__partials.alert')
+        @include('layouts.__partials.admin.breadcrumb')
+        <x-toggle-theme />
+        @yield('content')
+    </main>
+</body>
+@vite('resources/js/app.js')
+
+</html>
