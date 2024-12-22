62bfdbfbd78d2de1b2221a57d515258e2b8fc38b
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-22 12:05:29 -0600

    feat: Add view for creating new users with form validation and role selection

---
+++ resources/views/admin/usuarios/create.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.admin-template')
+@section('title', 'Nuevo becado')
+@section('content')
+    <section class="p-4">
+        <div class="flex items-center gap-2">
+            <x-icon icon="users" class="h-8 w-8 text-white" />
+            <h1 class="text-4xl font-bold text-white">
+                Nuevo usuario
+            </h1>
+        </div>
+        <div class="mt-4">
+            <form action="{{ Route('admin.usuarios.store') }}" method="POST">
+                @csrf
+                <div class="flex gap-4">
+                    <div class="flex flex-[2] flex-col">
+                        <x-input type="email" name="email" icon="email" label="Correo electrónico"
+                            placeholder="example@example.com" />
+                    </div>
+                    <div class="flex flex-1 flex-col">
+                        <x-input type="text" name="user" icon="user" label="Usuario"
+                            placeholder="Ingresa el nombre del usuario" />
+                    </div>
+                </div>
+                <div class="mt-4 flex gap-4">
+                    <div class="flex flex-1 flex-col">
+                        <x-input type="password" name="password" icon="lock" label="Contraseña"
+                            placeholder="Editar la contraseña del usuario" />
+                    </div>
+                    <div class="flex flex-1 flex-col">
+                        <x-input type="password" name="confirm-password" icon="" label="Confirmar contraseña"
+                            placeholder="Confirmar la contraseña del usuario" />
+                    </div>
+                </div>
+                <div class="mt-4 flex gap-4">
+                    <div class="w-60">
+                        <x-select label="Rol" :options="[
+                            'admin' => 'Administrador',
+                            'user' => 'Usuario',
+                        ]" id="role" name="role" />
+                    </div>
+                </div>
+                <div class="mt-4 flex items-center justify-center gap-4">
+                    <x-button type="a" href="{{ Route('admin.usuarios.index') }}" text="Cancelar" icon="close"
+                        typeButton="secondary" />
+                    <x-button type="submit" text="Guardar" icon="save" typeButton="primary" />
+                </div>
+            </form>
+        </div>
+    </section>
+@endsection
