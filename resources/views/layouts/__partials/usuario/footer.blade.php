acfd882defc2e7d9c8b66a8d10ab0e1a288e72f9
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-24 16:10:39 -0600

    feat: Improved user footer with responsive design and theme switching support

--- resources/views/layouts/__partials/usuario/footer.blade.php
+++ resources/views/layouts/__partials/usuario/footer.blade.php
@@ -1,15 +1,20 @@
-<footer class="py-4">
-    <div class="flex items-start justify-between py-4 text-sm text-gray-500 dark:text-gray-400">
+<footer>
+    <div
+        class="flex flex-col items-center justify-between gap-y-4 py-4 text-sm text-gray-500 dark:text-gray-400 sm:flex-row sm:items-start">
         <p>&copy; {{ date('Y') }} Todos los derechos reservados</p>
         <p>
             Centro de Intercambio y Solidaridad
         </p>
     </div>
-    <div class="mb-4 flex items-center justify-center gap-1 text-xs text-gray-500 dark:text-gray-400">
-        <p>
-            Desarrollado por
-        </p>
-        <a href="https://github.com/WillianArevalo" class="text-blue-500 hover:underline" target="_blank">Willian
-            Arévalo</a>
+    <div class="mb-4 flex items-center justify-between gap-1 text-xs text-gray-500 dark:text-gray-400">
+        <div class="flex items-center gap-1">
+            <p>
+                Desarrollado por
+            </p>
+            <a href="https://github.com/WillianArevalo" class="text-blue-500 hover:underline" target="_blank">Willian
+                Arévalo
+            </a>
+        </div>
+        <x-toggle-theme />
     </div>
 </footer>
