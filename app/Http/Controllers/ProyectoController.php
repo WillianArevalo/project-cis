45da4ad349e1dbb2981dbfe404a224487164849b
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-23 20:11:19 -0600

    feat: Adding features for project reporting management

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -4,17 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Models\Community;
 use App\Models\Project;
+use App\Models\Report;
 use App\Models\Scholarship;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 class ProyectoController extends Controller
 {
+
+    public function __construct()
+    {
+        Carbon::setLocale("es");
+    }
+
     public function index()
     {
         $proyectos = Project::with('community', 'scholarships')->get();
         $comunidades = Community::all();
-        return view('admin.proyectos.index', compact('proyectos', 'comunidades'));
+        $currentMonth =
+            Carbon::now()->translatedFormat("F");
+        return view('admin.proyectos.index', compact('proyectos', 'comunidades', 'currentMonth'));
     }
 
     public function store(Request $request)
@@ -38,13 +49,13 @@ class ProyectoController extends Controller
             DB::rollBack();
             return redirect()->back()
                 ->with('error_title', 'Error al guardar el proyecto')
-                ->with('error_message', $e->getMessage());
+                ->with('error_message', 'El proyecto no se ha guardado correctamente');
         }
     }
 
     public function edit($id)
     {
-        $proyecto = Project::findOrFail($id);
+        $proyecto = Project::with("community")->findOrFail($id);
         return response()->json($proyecto);
     }
 
@@ -59,6 +70,7 @@ class ProyectoController extends Controller
         try {
             $proyecto = Project::findOrFail($id);
             $proyecto->name = $request->name;
+            $proyecto->slug = Str::slug($request->name);
             $proyecto->community_id = $request->community_id;
             $proyecto->save();
             DB::commit();
@@ -78,6 +90,10 @@ class ProyectoController extends Controller
         DB::beginTransaction();
         try {
             $proyecto = Project::findOrFail($id);
+
+            Scholarship::where('project_id', $proyecto->id)
+                ->update(['project_id' => null]);
+
             $proyecto->delete();
             DB::commit();
             return redirect()->route('admin.proyectos.index')
@@ -91,12 +107,12 @@ class ProyectoController extends Controller
         }
     }
 
-    public function assign(string $id)
+    public function assign(string $slug)
     {
-        $proyecto = Project::with('scholarships')->findOrFail($id);
-        $becados = Scholarship::where(function ($query) use ($id) {
+        $proyecto = Project::with('scholarships')->where('slug', $slug)->first();
+        $becados = Scholarship::where(function ($query) use ($proyecto) {
             $query->whereNull('project_id')
-                ->orWhere('project_id', '!=', $id);
+                ->orWhere('project_id', '!=', $proyecto->id);
         })->get();
         return view('admin.proyectos.asignar', compact('proyecto', 'becados'));
     }
@@ -129,5 +145,19 @@ class ProyectoController extends Controller
                 ->with('error_title', 'Error al asignar los becados')
                 ->with('error_message', $e->getMessage());
         }
+    }
+
+    public function reportes(string $slug)
+    {
+        $proyecto = Project::with('scholarships')->where('slug', $slug)->first();
+        $reportes = Report::where('project_id', $proyecto->id)->get();
+        return view('admin.proyectos.reportes', compact('proyecto', 'reportes'));
+    }
+
+    public function showReport(string $id)
+    {
+        $user = auth()->user();
+        $report = Report::findOrFail($id);
+        return view("admin.proyectos.show-report", compact("report"));
     }
 }
