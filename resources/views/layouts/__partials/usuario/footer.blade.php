c8f9c4b83ba30dfcc243665a90d92f5ef1933f51
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-23 20:16:36 -0600

    feat: Add footer with user rights and credit information

---
+++ resources/views/layouts/__partials/usuario/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="py-4">
+    <div class="flex items-start justify-between py-4 text-sm text-gray-500 dark:text-gray-400">
+        <p>&copy; {{ date('Y') }} Todos los derechos reservados</p>
+        <p>
+            Centro de Intercambio y Solidaridad
+        </p>
+    </div>
+    <div class="mb-4 flex items-center justify-center gap-1 text-xs text-gray-500 dark:text-gray-400">
+        <p>
+            Desarrollado por
+        </p>
+        <a href="https://github.com/WillianArevalo" class="text-blue-500 hover:underline" target="_blank">Willian
+            Arévalo</a>
+    </div>
+</footer>
