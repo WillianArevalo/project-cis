bf2858a9cca985e60da13e173c54b2ab93247a9a
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-24 16:09:14 -0600

    feat: Add support for displaying icon in password field and improve error message display

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -21,9 +21,9 @@
 
     // Construir clases dinámicas para el input
     $classes = collect([
-        'bg-zinc-50 border border-zinc-400 text-zinc-900 text-sm rounded-xl  block w-full p-3 px-4  ',
+        'bg-zinc-50 border border-zinc-400 text-zinc-900 text-sm rounded-xl  block w-full p-2.5 px-5',
         'focus:ring-4 focus:ring-zinc-200 focus:border-zinc-600',
-        'dark:bg-zinc-950 dark:border-zinc-800 dark:placeholder-zinc-400 dark:text-white',
+        'dark:bg-transparent dark:border-zinc-800 dark:placeholder-zinc-400 dark:text-white',
         'dark:focus:ring-zinc-950 dark:focus:border-zinc-500',
         'transition duration-300',
         $class,
@@ -35,7 +35,7 @@
 
 @if ($label && $type !== 'checkbox' && $type !== 'radio' && $type !== 'file')
     <label for="{{ $id }}"
-        class="{{ $labelClass }} mb-2 block text-sm font-medium text-zinc-500 dark:text-zinc-300">
+        class="{{ $labelClass }} mb-1 block text-sm font-medium text-zinc-500 dark:text-zinc-300">
         {{ $label }}
     </label>
 @endif
@@ -62,6 +62,31 @@
             class="{{ $labelClass }} ms-1 inline-block text-sm font-medium text-zinc-500 dark:text-zinc-300">
             {{ $label }}
         </label>
+    @elseif ($type === 'password')
+        <div class="relative w-full">
+            @if ($icon)
+                <div class="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3.5">
+                    <span class="font-medium text-zinc-500 dark:text-zinc-400">
+                        <x-icon icon="{{ $icon }}" class="h-5 w-5 text-current" />
+                    </span>
+                </div>
+            @endif
+
+            <input type="password" name="{{ $name }}" id="{{ $id }}"
+                data-container=".error-{{ $name }}" placeholder="{{ $placeholder }}"
+                value="{{ $value ?? old($name) }}" class="{{ $classes }} {{ $icon ? 'ps-10' : '' }} pe-10"
+                {{ $attributes }} @if ($required) required @endif>
+
+            <button type="button"
+                class="toggle-password absolute inset-y-0 end-0 flex items-center pe-3 text-zinc-500 dark:text-zinc-400">
+                <span id="eye-icon">
+                    <x-icon icon="eye" class="h-5 w-5 text-current" />
+                </span>
+                <span id="eye-closed-icon" class="hidden">
+                    <x-icon icon="eye-closed" class="h-5 w-5 text-current" />
+                </span>
+            </button>
+        </div>
     @else
         <input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}"
             data-container=".error-{{ $name }}" placeholder="{{ $placeholder }}"
@@ -77,6 +102,11 @@
 
 @if ($error === true)
     @error($name)
-        <span class="text-sm text-red-500">{{ $message }}</span>
+        <span class="mt-2 flex items-center gap-1 text-sm text-red-500">
+            <x-icon icon="exclamation-circle" class="h-4 w-4" />
+            <span class="error-msg text-red-500">
+                {{ $message }}
+            </span>
+        </span>
     @enderror
 @endif
