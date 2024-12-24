9f3081b5147eceeb1f40e4c7ce89dff21c6282b6
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-23 20:12:16 -0600

    feat: Add unique slug column to projects table

--- database/migrations/2024_06_12_000356_create_projects_table.php
+++ database/migrations/2024_06_12_000356_create_projects_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string("slug")->unique();
             $table->foreignId('community_id')->constrained();
             $table->timestamps();
         });
