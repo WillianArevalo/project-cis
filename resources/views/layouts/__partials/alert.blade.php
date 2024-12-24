e2768defe01847936429b0ed8ba03faa7d5ffbf3
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-24 16:10:19 -0600

    feat: Add support for warning messages in the alert component

--- resources/views/layouts/__partials/alert.blade.php
+++ resources/views/layouts/__partials/alert.blade.php
@@ -8,18 +8,25 @@
 
     if (Session::has('success_title') && Session::has('success_message')) {
         $alertType = 'success';
-        $icon = 'check-circle'; // Icono para éxito
+        $icon = 'circle-check'; // Icono para éxito
         $bgColor = 'bg-green-500';
         $textColor = 'text-green-500 dark:text-green-400';
         $title = Session::get('success_title');
         $message = Session::get('success_message');
     } elseif (Session::has('error_title') && Session::has('error_message')) {
         $alertType = 'error';
-        $icon = 'x-circle'; // Icono para error
+        $icon = 'exclamation-circle'; // Icono para error
         $bgColor = 'bg-red-500';
         $textColor = 'text-red-500 dark:text-red-400';
         $title = Session::get('error_title');
         $message = Session::get('error_message');
+    } elseif (Session::has('warning_title') && Session::has('warning_message')) {
+        $alertType = 'warning';
+        $icon = 'info-circle'; // Icono para advertencia
+        $bgColor = 'bg-yellow-500';
+        $textColor = 'text-yellow-500 dark:text-yellow-400';
+        $title = Session::get('warning_title');
+        $message = Session::get('warning_message');
     }
 @endphp
 
@@ -27,28 +34,20 @@
     <div
         class="alert fixed right-10 top-10 z-[100] flex w-full max-w-sm animate-fade-left overflow-hidden rounded-lg bg-white shadow-md animate-duration-300 dark:bg-zinc-950">
         <div class="{{ $bgColor }} flex w-12 items-center justify-center">
-            <svg class="h-6 w-6 fill-current text-white" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-                @if ($alertType === 'success')
-                    <path
-                        d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z" />
-                @elseif ($alertType === 'error')
-                    <path
-                        d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z" />
-                @endif
-            </svg>
+            <x-icon icon="{{ $icon }}" class="size-6 min-w-6 max-w-6 text-white" />
         </div>
         <div class="-mx-3 px-4 py-2">
             <div class="mx-3 flex items-center justify-between">
                 <div>
-                    <span class="{{ $textColor }} font-semibold">
+                    <span class="{{ $textColor }} text-sm font-semibold sm:text-base">
                         {{ $title }}
                     </span>
-                    <p class="text-sm text-zinc-600 dark:text-zinc-200">
+                    <p class="text-xs text-zinc-600 dark:text-zinc-200 sm:text-sm">
                         {{ $message }}
                     </p>
                 </div>
                 <x-button type="button" icon="close" typeButton="secondary" onlyIcon
-                    class="alert-close {{ $textColor }} ms-4" />
+                    class="alert-close {{ $textColor }} ms-4" size="small" />
             </div>
         </div>
     </div>
