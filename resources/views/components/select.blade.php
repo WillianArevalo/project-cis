c11bde8605fb05bebcadcd3f021c6a52698819d4
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-23 20:15:44 -0600

    feat: Update select component styles for improved appearance and consistency

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -8,22 +8,22 @@
     <input type="hidden" id="{{ $id }}" name="{{ $name }}" value="{{ $value }}">
     <div class="relative">
         <div
-            class="selected @error($name) is-invalid @enderror flex w-full items-center justify-between rounded-lg border border-zinc-400 bg-zinc-50 px-4 py-2.5 text-sm dark:border-zinc-800 dark:bg-zinc-950 dark:text-white">
+            class="selected @error($name) is-invalid @enderror flex w-full items-center justify-between rounded-xl border border-zinc-400 bg-zinc-50 px-4 py-3 text-sm dark:border-zinc-800 dark:bg-zinc-950 dark:text-white">
             <span class="itemSelected truncate" id="{{ $id }}_selected">
                 {{ $selected && isset($options[$selected]) ? $options[$selected] : ($text ?: 'Seleccionar') }}
             </span>
             <x-icon icon="arrow-down" class="h-5 w-5 text-zinc-500 dark:text-white" />
         </div>
         <ul
-            class="selectOptions {{ count($options) > 6 ? 'h-64 overflow-auto' : '' }} absolute z-10 mb-8 mt-2 hidden w-full rounded-lg border border-zinc-400 bg-white p-2 shadow-lg dark:border-zinc-800 dark:bg-zinc-950">
+            class="selectOptions {{ count($options) > 6 ? 'h-64 overflow-auto' : '' }} absolute z-10 mb-8 mt-2 hidden w-full rounded-xl border border-zinc-400 bg-white p-2 shadow-lg dark:border-zinc-800 dark:bg-zinc-950">
             @if (count($options) === 0)
                 <li
-                    class="itemOption pointer-events-none rounded-lg px-4 py-2.5 text-sm text-zinc-900 dark:text-white dark:hover:bg-zinc-900">
+                    class="itemOption pointer-events-none rounded-xl px-4 py-3 text-sm text-zinc-900 dark:text-white dark:hover:bg-zinc-900">
                     No hay opciones disponibles
                 </li>
             @else
                 @foreach ($options as $value => $label)
-                    <li class="itemOption cursor-default truncate rounded-lg px-4 py-2.5 text-sm text-zinc-900 hover:bg-zinc-100 dark:text-white dark:hover:bg-zinc-900"
+                    <li class="itemOption cursor-default truncate rounded-xl px-4 py-3 text-sm text-zinc-900 hover:bg-zinc-100 dark:text-white dark:hover:bg-zinc-900/50"
                         title="{{ $label }}" data-value="{{ $value }}"
                         data-input="#{{ $id }}">
                         {{ $label }}
