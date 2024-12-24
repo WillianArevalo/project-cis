bfcd971cae37c5eaf75a89533bcab0836a032c33
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-24 16:04:57 -0600

    feat: Add NavAdminProvider for dynamic navbar data injection in admin layout

---
+++ app/Providers/NavAdminProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Setting;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
+class NavAdminProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        View::composer("layouts.__partials.admin.navbar", function ($view) {
+            $user = auth()->user();
+            $maintenance = Setting::where('key', 'maintenance')->first();
+            $project_mode = Setting::where('key', 'project_mode')->first();
+            $view->with([
+                'user' => $user,
+                'maintenance' => $maintenance,
+                'project_mode' => $project_mode,
+            ]);
+        });
+    }
+}
