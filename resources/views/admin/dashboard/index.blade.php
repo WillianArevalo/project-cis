a68cd47789ce037e35ea24b0bd441315187d4622
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-23 20:14:23 -0600

    feat: Improve the design of the admin panel with adjustments to the style and structure of components

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -3,82 +3,82 @@
 @section('content')
     <section class="p-4">
         <div class="flex items-center gap-2">
-            <x-icon icon="dashboard" class="h-8 w-8 text-white" />
-            <h1 class="text-4xl font-bold text-white">
+            <x-icon icon="dashboard" class="h-8 w-8 text-zinc-800 dark:text-white" />
+            <h1 class="text-4xl font-bold text-zinc-800 dark:text-white">
                 Dashboard
             </h1>
         </div>
-        <div class="mt-4 grid grid-cols-3 gap-4">
-            <div class="rounded-3xl border border-zinc-400 p-6 dark:border-zinc-800 dark:bg-zinc-950">
-                <div class="flex items-center justify-center gap-4">
-                    <span>
-                        <x-icon icon="school" class="h-8 w-8 text-black dark:text-white" />
+        <div class="mt-4 grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
+            <div class="h-full rounded-3xl border border-zinc-400 p-4 dark:border-zinc-800 dark:bg-zinc-950">
+                <div class="flex items-center justify-center gap-2">
+                    <span class="rounded-xl bg-blue-100 p-2 text-blue-500">
+                        <x-icon icon="school" class="size-6 text-current" />
                     </span>
-                    <h2 class="text-4xl font-bold text-black dark:text-white">
+                    <h2 class="text-2xl font-bold text-black dark:text-white">
                         {{ $becados }}
                         Becados
                     </h2>
                 </div>
                 <div class="mt-6 p-4">
-                    <img src="{{ asset('svg/scholarships.svg') }}" alt="becados" class="mx-auto h-60 w-72">
+                    <img src="{{ asset('svg/scholarships.svg') }}" alt="becados" class="mx-auto h-60 w-full">
                 </div>
                 <div class="mt-6 flex items-center justify-center">
                     <x-button type="a" href="{{ route('admin.becados.index') }}" typeButton="primary"
-                        text="Ver becados" icon="users" />
+                        text="Ver becados" icon="school" />
                 </div>
             </div>
-            <div class="rounded-3xl border border-zinc-400 p-6 dark:border-zinc-800 dark:bg-zinc-950">
-                <div class="flex items-center justify-center gap-4">
-                    <span>
-                        <x-icon icon="folder" class="h-8 w-8 text-black dark:text-white" />
+            <div class="rounded-3xl border border-zinc-400 p-4 dark:border-zinc-800 dark:bg-zinc-950">
+                <div class="flex items-center justify-center gap-2">
+                    <span class="rounded-xl bg-blue-100 p-2 text-blue-500">
+                        <x-icon icon="folders" class="size-6 text-current" />
                     </span>
-                    <h2 class="text-3xl font-bold text-black dark:text-white">
+                    <h2 class="text-2xl font-bold text-black dark:text-white">
                         {{ $proyectos }}
-                        Proyectos sociales
+                        Proyectos
                     </h2>
                 </div>
                 <div class="mt-6 p-4">
-                    <img src="{{ asset('svg/folders.svg') }}" alt="becados" class="mx-auto h-60 w-72">
+                    <img src="{{ asset('svg/folders.svg') }}" alt="becados" class="mx-auto h-60 w-full">
                 </div>
                 <div class="mt-6 flex items-center justify-center">
                     <x-button type="a" href="{{ route('admin.proyectos.index') }}" typeButton="primary"
                         text="Ver proyectos sociales" icon="folders" />
                 </div>
             </div>
-            <div class="rounded-3xl border border-zinc-400 p-6 dark:border-zinc-800 dark:bg-zinc-950">
-                <div class="flex items-center justify-center gap-4">
-                    <span>
-                        <x-icon icon="folder" class="h-8 w-8 text-black dark:text-white" />
+            <div class="rounded-3xl border border-zinc-400 p-4 dark:border-zinc-800 dark:bg-zinc-950">
+                <div class="flex items-center justify-center gap-2">
+                    <span class="rounded-xl bg-blue-100 p-2 text-blue-500">
+                        <x-icon icon="home" class="size-6 text-current" />
                     </span>
-                    <h2 class="text-3xl font-bold text-black dark:text-white">
+                    <h2 class="text-2xl font-bold text-black dark:text-white">
                         {{ $comunidades }}
                         Comunidades
                     </h2>
                 </div>
                 <div class="mt-6 p-4">
-                    <img src="{{ asset('svg/home.svg') }}" alt="becados" class="mx-auto h-60 w-72">
+                    <img src="{{ asset('svg/home.svg') }}" alt="becados" class="mx-auto h-60 w-full">
                 </div>
                 <div class="mt-6 flex items-center justify-center">
                     <x-button type="a" href="{{ route('admin.comunidades.index') }}" typeButton="primary"
                         text="Ver comunidades" icon="home" />
                 </div>
             </div>
-            <div class="rounded-3xl border border-zinc-400 p-6 dark:border-zinc-800 dark:bg-zinc-950">
-                <div class="flex items-center justify-center gap-4">
-                    <span>
-                        <x-icon icon="folder" class="h-8 w-8 text-black dark:text-white" />
+            <div class="rounded-3xl border border-zinc-400 p-4 dark:border-zinc-800 dark:bg-zinc-950">
+                <div class="flex items-center justify-center gap-2">
+                    <span class="rounded-xl bg-blue-100 p-2 text-blue-500">
+                        <x-icon icon="users" class="size-6 text-current" />
                     </span>
-                    <h2 class="text-3xl font-bold text-black dark:text-white">
+                    <h2 class="text-2xl font-bold text-black dark:text-white">
                         {{ $usuarios }}
                         Usuarios
                     </h2>
                 </div>
                 <div class="mt-6 p-4">
-                    <img src="{{ asset('svg/users.svg') }}" alt="becados" class="mx-auto h-60 w-72">
+                    <img src="{{ asset('svg/users.svg') }}" alt="becados" class="mx-auto h-60 w-full">
                 </div>
                 <div class="mt-6 flex items-center justify-center">
                     <x-button type="a" href="{{ route('admin.comunidades.index') }}" typeButton="primary"
-                        text="Ver comunidades" icon="home" />
+                        text="Ver usuarios" icon="users" />
                 </div>
             </div>
         </div>
