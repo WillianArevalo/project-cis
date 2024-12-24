07d697f8033934b599c61a4d41ccb55ad4daa06e
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-23 20:16:47 -0600

    feat: Improve the navigation bar with updated links and theme switching buttons

--- resources/views/layouts/__partials/usuario/navbar.blade.php
+++ resources/views/layouts/__partials/usuario/navbar.blade.php
@@ -5,17 +5,17 @@
     <nav class="flex justify-between">
         <div class="flex">
             <img src="{{ asset('images/cis-logo.webp') }}" alt="Logo CIS" class="h-10 w-10 rounded-full object-cover">
-            <ul class="ms-14 flex items-center gap-10 dark:text-white">
+            <ul class="ms-14 hidden items-center gap-10 dark:text-white sm:flex">
                 <li>
-                    <a href=""
-                        class="{{ Route::is('home') ? 'text-primary underline' : '' }} flex items-center gap-2 hover:text-primary hover:underline">
+                    <a href="{{ Route('home') }}"
+                        class="{{ Route::is('home') ? 'bg-blue-500 text-white' : '' }} hover:bg-blue flex items-center gap-2 rounded-full px-3 py-1 hover:underline">
                         <x-icon icon="home" class="h-5 w-5" />
                         Inicio
                     </a>
                 </li>
                 <li>
-                    <a href=""
-                        class="{{ Route::is('reports') ? 'text-primary underline' : '' }} flex items-center gap-2 hover:text-primary hover:underline">
+                    <a href="{{ Route('reporte.index') }}"
+                        class="{{ Route::is('reporte.index') ? 'bg-blue-500 text-white' : '' }} hover:bg-blue flex items-center gap-2 rounded-full px-3 py-1 hover:underline">
                         <x-icon icon="report" class="h-5 w-5" />
                         Reportes
                     </a>
@@ -24,8 +24,7 @@
         </div>
         <div class="flex gap-4">
             <div class="ms-2 hidden rounded-full border border-zinc-400 px-2 py-1 dark:border-zinc-800 sm:block">
-                <form action="" method="POST" class="flex items-center justify-center gap-2">
-                    @csrf
+                <div class="flex items-center justify-center gap-2">
                     <button type="button" data-theme="light"
                         class="theme-toggle theme-light flex items-center rounded-full p-2 hover:bg-zinc-200 dark:hover:bg-zinc-900">
                         <x-icon icon="sun" class="h-4 w-4 text-zinc-700 dark:text-white" />
@@ -34,19 +33,32 @@
                         class="theme-toggle theme-dark flex items-center rounded-full p-2 hover:bg-zinc-200 dark:hover:bg-zinc-900">
                         <x-icon icon="moon" class="h-4 w-4 text-zinc-700 dark:text-white" />
                     </button>
-                </form>
+                </div>
+            </div>
+            <div class="mb-2 ms-4 mt-2 block sm:hidden">
+                <div class="w-max rounded-lg border border-zinc-400 px-1.5 py-0.5 dark:border-zinc-800">
+                    <div class="flex items-center justify-center gap-2">
+                        <button type="button" data-theme="light"
+                            class="theme-toggle theme-light flex items-center rounded-lg p-1.5 hover:bg-zinc-200 dark:hover:bg-zinc-900">
+                            <x-icon icon="sun" class="h-3.5 w-3.5 text-zinc-700 dark:text-white" />
+                        </button>
+                        <button type="button" data-theme="dark"
+                            class="theme-toggle theme-dark flex items-center rounded-lg p-1.5 hover:bg-zinc-200 dark:hover:bg-zinc-900">
+                            <x-icon icon="moon" class="h-3.5 w-3.5 text-zinc-700 dark:text-white" />
+                        </button>
+                    </div>
+                </div>
             </div>
             <div class="relative">
                 <button type="button" class="flex items-center gap-2" id="profile-btn">
                     <img src="{{ Storage::url($user->scholarship->photo) }}" alt="{{ $user->name }}"
                         class="h-10 w-10 rounded-full object-cover">
                     <div class="flex flex-col">
-                        <span class="text-xs font-medium text-primary dark:text-primary">Hola,</span>
+                        <span class="text-xs font-medium text-blue-500">Hola,</span>
                         <span class="text-sm font-bold text-zinc-700 dark:text-white">{{ $user->user }}</span>
                     </div>
                     <x-icon icon="arrow-down" class="h-4 w-4 text-zinc-700 dark:text-zinc-400" />
                 </button>
-
                 <div class="absolute right-0 top-12 hidden w-44 rounded-xl border border-zinc-400 bg-white p-2 shadow-md dark:border-zinc-800 dark:bg-black"
                     id="profile-dropdown">
                     <ul class="flex flex-col">
