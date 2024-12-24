d8ff1ef053bf7bd3388a836bf184d3b0d1b21ab5
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-23 20:16:19 -0600

    feat: Update breadcrumb component styles for improved visual consistency

--- resources/views/layouts/__partials/admin/breadcrumb.blade.php
+++ resources/views/layouts/__partials/admin/breadcrumb.blade.php
@@ -4,13 +4,13 @@
     <div class="flex items-center justify-center border-b border-zinc-400 dark:border-zinc-800 sm:justify-end">
         <nav class="flex w-max px-0 py-3 text-zinc-700 sm:px-6" aria-label="Breadcrumb">
             <ol
-                class="inline-flex flex-wrap items-center justify-center gap-2 space-x-0 text-[10px] uppercase rtl:space-x-reverse sm:gap-0 sm:text-xs md:space-x-2">
+                class="inline-flex flex-wrap items-center justify-center gap-2 space-x-0 text-[10px] uppercase sm:gap-0 sm:text-xs md:space-x-2 rtl:space-x-reverse">
                 @foreach ($breadcrumbs as $key => $breadcrumb)
                     @if ($breadcrumb->url && !$loop->last)
                         <li>
                             <div class="flex items-center gap-1 sm:gap-4">
                                 <a href="{{ $breadcrumb->url }}"
-                                    class="flex items-center gap-1 rounded-lg px-2 py-1 font-semibold text-zinc-500 hover:bg-primary-50 hover:text-primary-500 dark:text-zinc-400 dark:hover:bg-primary-950 dark:hover:bg-opacity-40 dark:hover:text-primary-300">
+                                    class="flex items-center gap-1 rounded-lg px-2 py-1 font-semibold text-zinc-500 hover:bg-blue-50 hover:text-blue-500 dark:text-zinc-400 dark:hover:bg-blue-950 dark:hover:bg-opacity-40 dark:hover:text-blue-500">
                                     {!! $breadcrumb->title !!}
                                 </a>
                                 <x-icon icon="arrow-right" class="mx-1 block h-5 w-5 text-zinc-400 rtl:rotate-180" />
@@ -20,7 +20,7 @@
                         <li aria-current="page">
                             <div class="flex items-center">
                                 <span
-                                    class="ms-1 flex items-center gap-1 rounded-lg bg-primary-50 px-2 py-1 font-semibold text-primary-500 dark:bg-primary-950 dark:bg-opacity-40 dark:text-primary-300 md:ms-2">
+                                    class="ms-1 flex items-center gap-1 rounded-lg bg-blue-500 px-2 py-1 font-semibold text-white dark:bg-blue-950 dark:bg-opacity-40 dark:text-blue-500 md:ms-2">
                                     {!! $breadcrumb->title !!}
                                 </span>
                             </div>
