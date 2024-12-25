6910735ccdd9bbdb0ba90879c582e8ea596cf9a5
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-24 23:26:28 -0600

    feat: Update dashboard to display counts for becados, proyectos, comunidades, and usuarios

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -13,7 +13,7 @@
                         <x-icon icon="school" class="size-6 text-current" />
                     </span>
                     <h2 class="text-2xl font-bold text-black dark:text-white">
-                        {{ $becados }}
+                        {{ $countBecados }}
                         Becados
                     </h2>
                 </div>
@@ -31,7 +31,7 @@
                         <x-icon icon="folders" class="size-6 text-current" />
                     </span>
                     <h2 class="text-2xl font-bold text-black dark:text-white">
-                        {{ $proyectos }}
+                        {{ $countProyectos }}
                         Proyectos
                     </h2>
                 </div>
@@ -49,7 +49,7 @@
                         <x-icon icon="home" class="size-6 text-current" />
                     </span>
                     <h2 class="text-2xl font-bold text-black dark:text-white">
-                        {{ $comunidades }}
+                        {{ $countComunidades }}
                         Comunidades
                     </h2>
                 </div>
@@ -67,7 +67,7 @@
                         <x-icon icon="users" class="size-6 text-current" />
                     </span>
                     <h2 class="text-2xl font-bold text-black dark:text-white">
-                        {{ $usuarios }}
+                        {{ $countUsuarios }}
                         Usuarios
                     </h2>
                 </div>
@@ -78,6 +78,91 @@
                     <x-button type="a" href="{{ route('admin.comunidades.index') }}" typeButton="primary"
                         text="Ver usuarios" icon="users" />
                 </div>
+            </div>
+        </div>
+        <div class="mt-4">
+            <h2 class="text-lg font-bold text-zinc-800 dark:text-white sm:text-xl md:text-2xl">
+                Proyectos completados este mes ({{ $mes }})
+            </h2>
+            <div class="mt-4 flex flex-col gap-2">
+                <x-table id="tableProjects">
+                    <x-slot name="thead">
+                        <x-tr>
+                            <x-th>
+                                Proyecto
+                            </x-th>
+                            <x-th>
+                                Comunidad
+                            </x-th>
+                            <x-th>
+                                Reporte del mes
+                            </x-th>
+                            <x-th last="true" class="w-36">
+                                Enviado por
+                            </x-th>
+                        </x-tr>
+                    </x-slot>
+                    <x-slot name="tbody">
+                        @if ($proyectos->count() > 0)
+                            @foreach ($proyectos as $proyecto)
+                                <x-tr>
+                                    <x-td>
+                                        {{ $proyecto->name }}
+                                    </x-td>
+                                    <x-td>
+                                        {{ $proyecto->community->name }}
+                                    </x-td>
+                                    <x-td>
+                                        @if ($proyecto->reports->count() > 0)
+                                            @php
+                                                $reportThisMonth = $proyecto->reports->where('month', $mes)->first();
+                                            @endphp
+                                            @if ($reportThisMonth)
+                                                <a href="{{ route('admin.reportes.show', $reportThisMonth->id) }}"
+                                                    class="flex w-max items-center gap-1 rounded-full bg-emerald-100 px-2 py-1 text-xs text-emerald-500 dark:bg-emerald-950/30 dark:text-emerald-400">
+                                                    <x-icon icon="circle-check" class="h-4 w-4" />
+                                                    Reporte enviado
+                                                </a>
+                                            @else
+                                                <span
+                                                    class="flex w-max items-center gap-1 rounded-full bg-red-100 px-2 py-1 text-xs text-red-500 dark:bg-red-950/30 dark:text-red-400">
+                                                    <x-icon icon="info-circle" class="h-4 w-4" />
+                                                    Reporte pendiente
+                                                </span>
+                                            @endif
+                                        @else
+                                            <span
+                                                class="flex w-max items-center gap-1 rounded-full bg-red-100 px-2 py-1 text-xs text-red-500 dark:bg-red-950/30 dark:text-red-400">
+                                                <x-icon icon="info-circle" class="h-4 w-4" />
+                                                No hay reportes
+                                            </span>
+                                        @endif
+                                    </x-td>
+                                    <x-td>
+                                        @if ($proyecto->reports->count() > 0)
+                                            @php
+                                                $reportThisMonth = $proyecto->reports->where('month', $mes)->first();
+                                            @endphp
+                                            @if ($reportThisMonth)
+                                                <div class="flex -space-x-2 rtl:space-x-reverse">
+                                                    <img src="{{ Storage::url($reportThisMonth->user->scholarship->photo) }}"
+                                                        alt="{{ $proyecto->sentBy->user }}"
+                                                        class="h-8 w-8 rounded-full object-cover"
+                                                        data-tooltip-target="tooltip-user-{{ $reportThisMonth->id }}">
+                                                    <div id="tooltip-user-{{ $reportThisMonth->id }}" role="tooltip"
+                                                        class="tooltip invisible absolute z-10 inline-block rounded-lg bg-zinc-900 px-3 py-2 text-xs font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-zinc-900">
+                                                        {{ $reportThisMonth->user->scholarship->name }}
+                                                        <div class="tooltip-arrow" data-popper-arrow></div>
+                                                    </div>
+                                                </div>
+                                            @endif
+                                        @endif
+                                    </x-td>
+                                </x-tr>
+                            @endforeach
+                        @endif
+                    </x-slot>
+                </x-table>
             </div>
         </div>
     </section>
