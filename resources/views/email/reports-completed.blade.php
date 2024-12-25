e308614416031f0acd1d292a106ae4eabd40d9fd
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-24 23:27:26 -0600

    feat: Add email template for completed reports

---
+++ resources/views/email/reports-completed.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CIS | Reportes completados</title>
+</head>
+
+<body>
+    <div>
+        <img src="{{ asset('images/cis-logo.webp') }}" alt="Logo CIS"
+            style="width:80px; height:80px; object-fit:cover; margin-inline:auto;">
+    </div>
+    <h1 style="text-align:center; font-size:1.5rem; font-weight:bold; color:#333; margin-top:10px;">
+        Reportes del mes de {{ $mes }} completados
+    </h1>
+</body>
+
+</html>
