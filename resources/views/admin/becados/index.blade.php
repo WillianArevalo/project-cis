831ac91ca3b84f62ee1368a7d850266bc6e3dbc6
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-24 23:26:24 -0600

    feat: Display count of becados and add role labels in the becados list

--- resources/views/admin/becados/index.blade.php
+++ resources/views/admin/becados/index.blade.php
@@ -2,7 +2,10 @@
 @section('title', 'Becados')
 @section('content')
     <section class="p-4">
-        @include('layouts.__partials.admin.header', ['title' => 'Becados', 'icon' => 'school'])
+        @include('layouts.__partials.admin.header', [
+            'title' => 'Becados' . ' (' . $becados->count() . ')',
+            'icon' => 'school',
+        ])
         <div class="mt-4 flex flex-col gap-4 sm:flex-row">
             <div class="flex-1">
                 <x-input type="text" placeholder="Buscar becado" icon="search" id="inputSearchScholarships" />
@@ -54,7 +57,22 @@
                                     class="h-10 w-10 min-w-10 max-w-10 rounded-full object-cover">
                             </x-td>
                             <x-td>
-                                {{ $becado->name }}
+                                <div class="flex flex-col gap-1">
+                                    {{ $becado->name }}
+                                    @if ($becado->user->role === 'becado')
+                                        <span
+                                            class="flex w-max items-center gap-1 rounded-full bg-green-200 px-2 py-1 text-xs font-medium text-green-700 dark:bg-green-950/30 dark:text-green-500">
+                                            <x-icon icon="school" class="h-4 w-4" />
+                                            Becado
+                                        </span>
+                                    @else
+                                        <span
+                                            class="flex w-max items-center gap-1 rounded-full bg-blue-200 px-2 py-1 text-xs font-medium text-blue-700 dark:bg-blue-950/30 dark:text-blue-500">
+                                            <x-icon icon="user" class="h-4 w-4" />
+                                            Administrador
+                                        </span>
+                                    @endif
+                                </div>
                             </x-td>
                             <x-td>
                                 {{ $becado->institution }}
