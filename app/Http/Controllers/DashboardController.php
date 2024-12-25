f334511a17d85aac455172832bdc197246628401
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-24 23:24:35 -0600

    feat: Enhance dashboard data retrieval with monthly reports and project listing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,18 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\Community;
 use App\Models\Project;
+use App\Models\Report;
 use App\Models\Scholarship;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+
 
 class DashboardController extends Controller
 {
+    public function __construct()
+    {
+        Carbon::setLocale("es");
+    }
+
     public function index()
     {
-        $usuarios = User::count();
-        $becados = Scholarship::count();
-        $proyectos = Project::count();
-        $comunidades = Community::count();
-        return view("admin.dashboard.index", compact("usuarios", "becados", "proyectos", "comunidades"));
+        $countUsuarios = User::count();
+        $countBecados = Scholarship::count();
+        $countProyectos = Project::count();
+        $countComunidades = Community::count();
+        $mes = Carbon::now()->translatedFormat("F");
+        $reportes = Report::where("month", $mes)->get();
+        $proyectos = Project::all();
+
+        return view("admin.dashboard.index", [
+            "countUsuarios" => $countUsuarios,
+            "countBecados" => $countBecados,
+            "countProyectos" => $countProyectos,
+            "countComunidades" => $countComunidades,
+            "reportes" => $reportes,
+            "mes" => $mes,
+            "proyectos" => $proyectos
+        ]);
     }
 }
