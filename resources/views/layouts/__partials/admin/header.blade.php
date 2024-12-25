ad7c7d47cb0e52efca1ff91cdc322113b336986b
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-25 00:11:38 -0600

    fix: Ajustar tamaño del título en el encabezado para mejorar la legibilidad

--- resources/views/layouts/__partials/admin/header.blade.php
+++ resources/views/layouts/__partials/admin/header.blade.php
@@ -2,7 +2,7 @@
 
  <div class="flex items-center gap-2">
      <x-icon icon="{{ $icon }}" class="size-7 text-zinc-800 dark:text-white sm:size-8 md:size-10" />
-     <h1 class="text-xl font-bold text-zinc-800 dark:text-white sm:text-2xl md:text-3xl lg:text-4xl">
+     <h1 class="text-lg font-bold text-zinc-800 dark:text-white sm:text-xl md:text-2xl lg:text-3xl">
          {{ $title }}
      </h1>
  </div>
