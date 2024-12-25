2985bbcf6bf402c4ac387365db0c1bd1b8228677
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-24 23:29:20 -0600

    fix: Improve accessibility in pagination by adding cursor classes

--- resources/views/vendor/pagination/pagination-custom.blade.php
+++ resources/views/vendor/pagination/pagination-custom.blade.php
@@ -13,7 +13,7 @@
             @if ($paginator->onFirstPage())
                 <li aria-disabled="true" aria-label="@lang('pagination.previous')">
                     <span
-                        class="ml-0 flex h-full items-center justify-center rounded-l-lg border border-zinc-400 bg-white px-3 py-1.5 text-zinc-500 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-400">
+                        class="ml-0 flex h-full cursor-not-allowed items-center justify-center rounded-l-lg border border-zinc-400 bg-white px-3 py-1.5 text-zinc-500 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-400">
                         <span class="sr-only">Previous</span>
                         <svg class="h-5 w-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg">
@@ -50,7 +50,7 @@
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
                             <li aria-current="page"><span
-                                    class="border-primary-300 z-10 flex items-center justify-center border bg-blue-50 bg-blue-600 px-3 py-2 text-sm leading-tight dark:border-zinc-800 dark:bg-zinc-950 dark:text-white">{{ $page }}</span>
+                                    class="border-primary-300 z-10 flex items-center justify-center border bg-blue-600 px-3 py-2 text-sm leading-tight text-white dark:border-zinc-800 dark:bg-blue-700 dark:text-white">{{ $page }}</span>
                             </li>
                         @else
                             <li><a href="{{ $url }}"
@@ -78,7 +78,7 @@
             @else
                 <li aria-disabled="true" aria-label="@lang('pagination.next')">
                     <span
-                        class="flex h-full items-center justify-center rounded-r-lg border border-zinc-400 bg-white px-3 py-1.5 leading-tight text-zinc-500 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-400">
+                        class="flex h-full cursor-not-allowed items-center justify-center rounded-r-lg border border-zinc-400 bg-white px-3 py-1.5 leading-tight text-zinc-500 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-400">
                         <span class="sr-only">Next</span>
                         <svg class="h-5 w-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg">
