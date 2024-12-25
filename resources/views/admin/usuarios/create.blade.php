1bceac2b679699c913a230cd784ce437ba161864
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-25 00:27:47 -0600

    fix: Improve the layout of the user creation form for better display on small screens

--- resources/views/admin/usuarios/create.blade.php
+++ resources/views/admin/usuarios/create.blade.php
@@ -6,7 +6,7 @@
         <div class="mt-4">
             <form action="{{ Route('admin.usuarios.store') }}" method="POST">
                 @csrf
-                <div class="flex gap-4">
+                <div class="flex flex-col gap-4 sm:flex-row">
                     <div class="flex flex-[2] flex-col">
                         <x-input type="email" name="email" icon="email" label="Correo electrónico"
                             placeholder="example@example.com" />
@@ -16,28 +16,28 @@
                             placeholder="Ingresa el nombre del usuario" />
                     </div>
                 </div>
-                <div class="mt-4 flex gap-4">
+                <div class="mt-4 flex flex-col gap-4 sm:flex-row">
                     <div class="flex flex-1 flex-col">
-                        <x-input type="password" name="password" icon="lock" label="Contraseña"
+                        <x-input type="password" name="password" icon="lock" label="Contraseña" id="new-password"
                             placeholder="Editar la contraseña del usuario" />
                     </div>
                     <div class="flex flex-1 flex-col">
-                        <x-input type="password" name="confirm-password" icon="" label="Confirmar contraseña"
-                            placeholder="Confirmar la contraseña del usuario" />
+                        <x-input type="password" name="confirm-password" icon="lock" id="confirm-password"
+                            label="Confirmar contraseña" placeholder="Confirmar la contraseña del usuario" />
                     </div>
                 </div>
                 <div class="mt-4 flex gap-4">
-                    <div class="w-60">
+                    <div class="w-full sm:w-60">
                         <x-select label="Rol" :options="[
                             'admin' => 'Administrador',
                             'user' => 'Usuario',
                         ]" id="role" name="role" />
                     </div>
                 </div>
-                <div class="mt-4 flex items-center justify-center gap-4">
+                <div class="mt-4 flex flex-col items-center justify-center gap-4 sm:flex-row">
                     <x-button type="a" href="{{ Route('admin.usuarios.index') }}" text="Cancelar" icon="close"
-                        typeButton="secondary" />
-                    <x-button type="submit" text="Guardar" icon="save" typeButton="primary" />
+                        typeButton="secondary" class="w-full sm:w-max" />
+                    <x-button type="submit" text="Guardar" icon="save" class="w-full sm:w-max" typeButton="primary" />
                 </div>
             </form>
         </div>
