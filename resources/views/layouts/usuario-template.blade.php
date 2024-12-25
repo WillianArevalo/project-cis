a9218a022d3d1289b4fd989401622b9e8862579d
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-25 00:34:50 -0600

    fix: style gradient

--- resources/views/layouts/usuario-template.blade.php
+++ resources/views/layouts/usuario-template.blade.php
@@ -34,7 +34,7 @@
     <div
         class="fixed inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] dark:hidden">
         <div
-            class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_400px_at_50%_300px,#C9EBFF,transparent)]">
+            class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_300px,#C9EBFF,transparent)]">
         </div>
     </div>
 
