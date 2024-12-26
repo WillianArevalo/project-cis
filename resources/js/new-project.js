51b63ba1916f8cb9cd0983209d29a22b453c8a69
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-26 17:36:20 -0600

    feat: Implement map capture and preview in new project module

--- resources/js/new-project.js
+++ resources/js/new-project.js
@@ -26,27 +26,63 @@ $(document).ready(function () {
         alert("Tu navegador no soporta geolocalización");
     }
 
-    $("#get-link").on("click", function () {
-        if (marker) {
-            const { lat, lng } = marker.getLatLng();
-            const link = `https://www.openstreetmap.org/?mlat=${lat}&mlon=${lng}#map=18/${lat}/${lng}`;
-            alert("Enlace de ubicación: " + link);
-        } else {
-            alert("Primero debes seleccionar una ubicación en el mapa");
-        }
-    });
+    $("#capture-map").on("click", function () {
+        $("#loader").removeClass("hidden");
+        leafletImage(map, function (err, canvas) {
+            $("#loader").addClass("hidden");
 
-    $("#capture-map").click(function () {
-        html2canvas($("#map")[0]).then((canvas) => {
-            canvas.toBlob((blob) => {
-                const file = new File([blob], "map-cature.png", {
-                    type: "image/png",
-                });
-                const dataTransfer = new DataTransfer();
-                dataTransfer.items.add(file);
-                $("#map-capture")[0].files = dataTransfer.files;
-                $("#preview-map").attr("src", URL.createObjectURL(file));
-            });
+            if (err) {
+                alert("Ocurrió un error al capturar el mapa");
+                console.error(err);
+                return;
+            }
+
+            if (marker) {
+                const { lat, lng } = marker.getLatLng();
+                const link = `https://www.openstreetmap.org/?mlat=${lat}&mlon=${lng}#map=18/${lat}/${lng}`;
+                $("#location").val(link);
+            } else {
+                alert("Primero debes seleccionar una ubicación en el mapa");
+            }
+
+            const imgData = canvas.toDataURL("image/png");
+            const bob = dataURItoBlob(imgData);
+            const file = new File([bob], "mapa.png", { type: "image/png" });
+
+            const dataTransfer = new DataTransfer();
+            dataTransfer.items.add(file);
+            document.getElementById("map-image").files = dataTransfer.files;
+
+            const preview = $("<img>")
+                .attr("src", imgData)
+                .addClass("img-fluid rounded-xl");
+            $("#preview-container").removeClass("hidden");
+            $("#preview-container").html(preview);
+
+            $("#map").addClass("hidden");
+            $("#capture-map-msg").removeClass("hidden");
+            $("#capture-map").addClass("hidden");
+            $("#remove-map").removeClass("hidden");
         });
     });
+
+    $("#remove-map").on("click", function () {
+        $("#map").removeClass("hidden");
+        $("#capture-map-msg").addClass("hidden");
+        $("#capture-map").removeClass("hidden");
+        $("#remove-map").addClass("hidden");
+        $("#preview-container").addClass("hidden");
+        $("#location").val("");
+    });
+
+    function dataURItoBlob(dataURI) {
+        const byteString = atob(dataURI.split(",")[1]);
+        const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+        const ab = new ArrayBuffer(byteString.length);
+        const ia = new Uint8Array(ab);
+        for (let i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+        return new Blob([ab], { type: mimeString });
+    }
 });
