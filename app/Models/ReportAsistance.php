8b16303081812505dcaee117d73e7745f686275c
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-26 16:25:54 -0600

    feat: Add ReportAsistance model for managing report and scholarship associations

---
+++ app/Models/ReportAsistance.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReportAsistance extends Model
+{
+    use HasFactory;
+
+    protected $table = 'report_assits';
+
+    protected $fillable = [
+        'report_id',
+        'scholarship_id',
+    ];
+
+    public function report()
+    {
+        return $this->belongsTo(Report::class);
+    }
+
+    public function scholarship()
+    {
+        return $this->belongsTo(Scholarship::class);
+    }
+}
