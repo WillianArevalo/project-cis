285ac2830c87d5e3eccf716dff085d3904db39c2
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-26 17:36:40 -0600

    feat: Update loader partial styles for improved visibility

--- resources/views/layouts/__partials/loader.blade.php
+++ resources/views/layouts/__partials/loader.blade.php
@@ -1,5 +1,5 @@
 <div id="loader" class="hidden">
-    <div class="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-white/50 dark:bg-black/50">
+    <div class="fixed left-0 top-0 z-[100] flex h-full w-full items-center justify-center bg-white/50 dark:bg-black/90">
         <div class="dot-spinner">
             <div class="dot-spinner__dot"></div>
             <div class="dot-spinner__dot"></div>
