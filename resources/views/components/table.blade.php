4b364ad23364655c4470ec88430856b161416e3d
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-29 14:21:42 -0600

    feat: Adjust overflow behavior in table component to improve display on large screens

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,5 +1,5 @@
 @props(['thead', 'tbody'])
-<div class="w-full overflow-x-auto rounded-xl border border-zinc-400 dark:border-zinc-800">
+<div class="w-full overflow-x-auto rounded-xl border border-zinc-400 dark:border-zinc-800 xl:overflow-x-hidden">
     <table {{ $attributes->merge(['class' => 'w-full text-left text-sm text-zinc-500 dark:text-zinc-400']) }}>
         <thead class="border-b border-zinc-400 text-xs uppercase text-zinc-700 dark:border-zinc-800 dark:text-zinc-300">
             {{ $thead ?? '' }}
