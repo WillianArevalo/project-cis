29efa6094b2e6ef70775f08ebc4f68ba8b447968
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-12-29 14:20:05 -0600

    feat: Enhance toast notifications with improved styling and dismiss button

--- resources/js/toast.js
+++ resources/js/toast.js
@@ -4,7 +4,7 @@ export function showToast(type, title, message) {
     const currentToastId = `toast-success-${toasCount++}`;
     const div = $(`<div id="${currentToastId}" role="alert"></div>`);
     div.addClass(
-        "alert fixed right-10 top-10 z-[100] flex w-full max-w-sm animate-fade-left overflow-hidden rounded-lg bg-white shadow-md animate-duration-300 dark:bg-zinc-950"
+        "alert fixed top-10 z-[100] flex w-full max-w-sm animate-fade-left overflow-hidden rounded-lg bg-white shadow-md animate-duration-300 dark:bg-zinc-950 sm:right-10"
     );
 
     let svg = "";
@@ -23,22 +23,18 @@ export function showToast(type, title, message) {
                 <path d="M12 12L12 8" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
             </svg>`;
         color = "red";
-    } else if (type === "info") {
+    } else if (type === "warning") {
         svg = `
-            <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
-                <path d="M22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12Z" stroke="currentColor" stroke-width="1.5" />
-                <path d="M12.2422 17V12C12.2422 11.5286 12.2422 11.2929 12.0957 11.1464C11.9493 11 11.7136 11 11.2422 11" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M11.992 8H12.001" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            </svg>`;
-        color = "blue";
+            <svg class="size-6 text-white" xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-info-circle"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 2c5.523 0 10 4.477 10 10a10 10 0 0 1 -19.995 .324l-.005 -.324l.004 -.28c.148 -5.393 4.566 -9.72 9.996 -9.72zm0 9h-1l-.117 .007a1 1 0 0 0 0 1.986l.117 .007v3l.007 .117a1 1 0 0 0 .876 .876l.117 .007h1l.117 -.007a1 1 0 0 0 .876 -.876l.007 -.117l-.007 -.117a1 1 0 0 0 -.764 -.857l-.112 -.02l-.117 -.006v-3l-.007 -.117a1 1 0 0 0 -.876 -.876l-.117 -.007zm.01 -3l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007z" /></svg>`;
+        color = "yellow";
     }
 
     div.html(`
         <div class="flex w-12 items-center justify-center bg-${color}-500">
             ${svg}
         </div>
-        <div class="-mx-3 px-4 py-2">
-            <div class="mx-3 flex items-center justify-between">
+        <div class="-mx-3 px-4 py-2 flex items-center justify-between w-full">
+            <div class="mx-3">
                 <div>
                     <span class="text-${color}-500 font-semibold">
                         ${title}
@@ -47,15 +43,19 @@ export function showToast(type, title, message) {
                         ${message}
                     </p>
                 </div>
-
             </div>
-        </div>
-       `);
+             <button type="button"
+                class="ms-auto me-2 bg-white text-zinc-400 hover:text-zinc-900 rounded-xl focus:ring-2 focus:ring-zinc-300  hover:bg-zinc-100 inline-flex items-center justify-center h-8 min-w-8"
+                data-dismiss-target="#${currentToastId}" aria-label="Close">
+                <span class="sr-only">Close</span>
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+              </button>
+        </div>`);
     toastContainer.append(div);
-
-    setTimeout(() => {
-        $(`#${currentToastId}`).remove();
-    }, 3500);
 
     $(`button[data-dismiss-target="#${currentToastId}"]`).click(function () {
         const targetId = $(this).attr("data-dismiss-target");
