33c7ab34039ac56ba9b73aacdeea75c8e21d6392
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2025-01-10 20:12:19 -0600

    feat: Add Ask model for managing question-related data and relationships

---
+++ app/Models/Ask.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ask extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'level',
+        'description',
+        'max_characters',
+    ];
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
+}
