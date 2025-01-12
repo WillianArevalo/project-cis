b1e4b417397f2ca4af2459a64d849cb33d7fcb4f
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2025-01-11 23:00:57 -0600

    feat: Simplify Card component constructor and remove unused properties

--- app/View/Components/Card.php
+++ app/View/Components/Card.php
@@ -2,29 +2,14 @@
 
 namespace App\View\Components;
 
-use Closure;
-use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class Card extends Component
 {
-    public $report;
-    public $month;
-    public $send;
-    /**
-     * Create a new component instance.
-     */
-    public function __construct($report, $month, $send = false)
-    {
-        $this->report = $report;
-        $this->month = $month;
-        $this->send = $send;
-    }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
-    public function render(): View|Closure|string
+    public function __construct() {}
+
+    public function render()
     {
         return view('components.card');
     }
