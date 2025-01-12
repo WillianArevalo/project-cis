62fe8b5fd509414ff8413043338eb76d466e5815
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2025-01-11 23:01:21 -0600

    feat: Update Card component to display report counts and improve styling

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,7 +1,6 @@
-@props(['month', 'send', 'report', 'index', 'limitMonth'])
+@props(['month', 'send', 'reports', 'index', 'limitMonth'])
 
 @php
-    // Definimos el estilo base para los reportes enviados y pendientes.
     $valid = $send ? 'success' : 'danger';
     $style = [
         'success' =>
@@ -11,7 +10,6 @@
             'bg-gray-100 text-gray-500 dark:bg-gray-950/30 dark:text-gray-300 border-gray-500 dark:border-gray-900',
     ];
 
-    // Obtener el índice del mes actual.
     $monthIndex = array_search($month, [
         'enero',
         'febrero',
@@ -27,7 +25,6 @@
         'diciembre',
     ]);
 
-    // Obtener el índice del mes límite.
     $limiteIndex = array_search($limitMonth, [
         'enero',
         'febrero',
@@ -43,13 +40,11 @@
         'diciembre',
     ]);
 
-    // Si el mes es posterior al mes límite, aplicamos un estilo de "deshabilitado".
     $disabled = $monthIndex > $limiteIndex;
+    $cardStyle = $disabled ? 'disabled' : $valid;
 
-    // Determinamos si el mes está dentro del límite o si es posterior (y debe tener un estilo diferente).
-    $cardStyle = $disabled ? 'disabled' : $valid; // Si el mes es posterior, usa el estilo 'disabled'.
+    $reportCount = count($reports);
 @endphp
-
 <div
     class="card {{ $style[$cardStyle] }} {{ $disabled ? 'opacity-50 cursor-not-allowed' : '' }} overflow-hidden rounded-xl border shadow-md">
     <div class="p-4 text-center">
@@ -57,47 +52,46 @@
             {{ $month }}
         </h3>
     </div>
-    <div class="{{ $style[$cardStyle] }} flex h-36 flex-col items-center justify-center gap-2 border-t p-4 text-center">
+    <div class="{{ $style[$cardStyle] }} flex h-32 flex-col items-center justify-center gap-4 border-t p-4 text-center">
         @if ($send)
-            <span
-                class="flex items-center gap-2 rounded-full bg-green-100 px-2 py-1 text-sm text-green-500 dark:bg-green-950/30 dark:text-green-300">
-                <x-icon icon="calendar-check" class="size-4 text-green-500" />
-                Reporte enviado
-            </span>
-            <p class="text-sm text-zinc-800 dark:text-white">
-                {{ $report->theme }}
-            </p>
-            <p class="text-sm text-zinc-800 dark:text-white">
-                {{ $report->created_at->setTimezone('America/El_Salvador')->format('M d, Y h:i A') }}
-            </p>
+            <div class="flex flex-col items-center gap-2">
+                <span
+                    class="flex items-center gap-1 rounded-full bg-green-100 px-2 py-1 text-sm text-green-500 dark:bg-green-950/30 dark:text-green-300">
+                    <x-icon icon="circle-check" class="size-4 text-green-500" />
+                    {{ $reportCount }} reporte{{ $reportCount > 1 ? 's' : '' }}
+                    enviado{{ $reportCount > 1 ? 's' : '' }}
+                </span>
+                <ul class="list-none text-left text-sm text-zinc-800 dark:text-zinc-300">
+                    @foreach ($reports as $report)
+                        <li class="mt-1 flex items-center gap-1 text-xs">
+                            <x-icon icon="calendar" class="size-4 text-zinc-500 dark:text-zinc-300" />
+                            {{ $report->created_at->locale('es')->setTimeZone('America/El_Salvador')->translatedFormat('d \d\e F \d\e Y h:i A') }}
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
         @else
             @if ($disabled)
                 <span
-                    class="flex h-full w-full items-center justify-center gap-2 px-2 py-1 text-sm text-gray-500 dark:text-gray-300">
-                    <x-icon icon="calendar-clock" class="size-4 text-gray-500" />
-                    Reporte pendiente
+                    class="flex h-full w-full items-center justify-center gap-2 px-2 py-1 text-xs text-gray-500 dark:text-gray-300">
+                    Todavía no puedes enviar reportes para este mes
                 </span>
             @else
                 <span
                     class="flex h-full w-full items-center justify-center gap-2 px-2 py-1 text-sm text-red-500 dark:text-red-300">
                     <x-icon icon="calendar-clock" class="size-4 text-red-500" />
-                    Reporte pendiente
+                    Reportes pendientes
                 </span>
             @endif
         @endif
     </div>
     <div class="{{ $style[$cardStyle] }} border-t p-4 text-center">
-        @if ($send)
-            <x-button type="a" href="{{ Route('reportes.show', ['mes' => $month]) }}" typeButton="success"
-                text="Ver reporte" icon="eye" />
+        @if ($disabled)
+            <x-button type="button" typeButton="secondary" text="Enviar reporte" icon="send"
+                class="w-full cursor-not-allowed" />
         @else
-            @if ($disabled)
-                <x-button type="button" typeButton="secondary" text="Enviar reporte" icon="send"
-                    class="w-full cursor-not-allowed" />
-            @else
-                <x-button type="a" href="{{ Route('reportes.create', ['mes' => $month]) }}" typeButton="primary"
-                    text="Enviar reporte" icon="send" :disabled="$disabled" />
-            @endif
+            <x-button type="a" href="{{ Route('reportes.create', ['mes' => $month]) }}" typeButton="primary"
+                text="Enviar reporte" icon="send" :disabled="$disabled" />
         @endif
     </div>
 </div>
