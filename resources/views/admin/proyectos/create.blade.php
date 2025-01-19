c6b5b3b31e19a5b095d0926a412d82028e7a80a4
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2025-01-18 20:20:43 -0600

    feat: Refactor file input fields for map, schedule, and project with improved components

--- resources/views/admin/proyectos/create.blade.php
+++ resources/views/admin/proyectos/create.blade.php
@@ -81,118 +81,17 @@
                     label="Ubicación" icon="map-pin" id="location" />
             </div>
             <div class="flex flex-col gap-4 md:flex-row md:items-center">
-
                 <div class="mt-4 flex-1">
-                    <span
-                        class="mb-1 block text-sm font-medium text-zinc-500 after:ml-0.5 after:text-red-500 after:content-['*'] dark:text-zinc-300">
-                        Mapa
-                    </span>
-                    <div
-                        class="flex flex-col items-start justify-between rounded-xl border border-dashed border-zinc-400 p-2 dark:border-zinc-800">
-                        <div class="flex w-full justify-between">
-                            <div class="flex items-center gap-2">
-                                <label
-                                    class="flex w-max cursor-pointer items-center justify-center gap-1 text-nowrap rounded-xl border border-zinc-400 bg-white px-4 py-2 text-xs text-zinc-600 transition-colors duration-300 hover:bg-zinc-200/50 disabled:cursor-not-allowed disabled:bg-zinc-100/50 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-200 dark:hover:bg-zinc-900/50 sm:text-xs">
-                                    <input type="file" name="map" id="input-map" class="input-doc hidden"
-                                        accept=".jpg, .png, .jpeg, .webp" data-button-remove="#remove-file-map"
-                                        data-name="#file-name-map" />
-                                    <x-icon icon="file" class="h-4 w-4 text-zinc-600 dark:text-zinc-300" />
-                                    Adjuntar mapa
-                                </label>
-                                <p id="file-name-map" class="font-dine-r text-[10px] text-zinc-600 dark:text-zinc-300">
-                                    Formatos permitidos: .jpg, .png, .jpeg, .webp
-                                </p>
-                            </div>
-                            <button class="remove-file hidden" id="remove-file-map" type="button"
-                                data-input="#input-map">
-                                <x-icon icon="close" class="h-4 w-4 text-red-500" />
-                            </button>
-                        </div>
-                    </div>
-                    @error('document')
-                        <span class="mt-2 flex items-center gap-1 text-sm text-red-500">
-                            <x-icon icon="exclamation-circle" class="h-4 w-4" />
-                            <span class="error-msg text-red-500">
-                                {{ $message }}
-                            </span>
-                        </span>
-                    @enderror
+                    <x-input type="file" name="map" id="map" icon="map" required
+                        accept=".jpg, .png, .jpeg, .webp" label="Mapa" placeholder="Adjuntar mapa" />
                 </div>
-
                 <div class="mt-4 flex-1">
-                    <span
-                        class="mb-1 block text-sm font-medium text-zinc-500 after:ml-0.5 after:text-red-500 after:content-['*'] dark:text-zinc-300">
-                        Cronograma
-                    </span>
-                    <div
-                        class="flex flex-col items-start justify-between rounded-xl border border-dashed border-zinc-400 p-2 dark:border-zinc-800">
-                        <div class="flex w-full justify-between">
-                            <div class="flex items-center gap-2">
-                                <label
-                                    class="flex w-max cursor-pointer items-center justify-center gap-1 text-nowrap rounded-xl border border-zinc-400 bg-white px-4 py-2 text-xs text-zinc-600 transition-colors duration-300 hover:bg-zinc-200/50 disabled:cursor-not-allowed disabled:bg-zinc-100/50 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-200 dark:hover:bg-zinc-900/50 sm:text-xs">
-                                    <input type="file" name="schedule" id="input-schedule" class="input-doc hidden"
-                                        accept=".pdf" data-name="#file-name-schedule"
-                                        data-button-remove="#remove-file-schedule" />
-                                    <x-icon icon="file" class="h-4 w-4 text-zinc-600 dark:text-zinc-300" />
-                                    Adjuntar cronograma
-                                </label>
-                                <p id="file-name-schedule"
-                                    class="font-dine-r text-[10px] text-zinc-600 dark:text-zinc-300">
-                                    Formatos permitidos: .pdf
-                                </p>
-                            </div>
-                            <button class="remove-file hidden" id="remove-file-schedule" type="button"
-                                data-input="#input-schedule">
-                                <x-icon icon="close" class="h-4 w-4 text-red-500" />
-                            </button>
-                        </div>
-                    </div>
-                    @error('document')
-                        <span class="mt-2 flex items-center gap-1 text-sm text-red-500">
-                            <x-icon icon="exclamation-circle" class="h-4 w-4" />
-                            <span class="error-msg text-red-500">
-                                {{ $message }}
-                            </span>
-                        </span>
-                    @enderror
+                    <x-input type="file" name="schedule" id="schedule" icon="calendar" required accept=".pdf, .docx"
+                        label="Cronograma" placeholder="Adjuntar cronograma" />
                 </div>
-
                 <div class="mt-4 flex-1">
-                    <span
-                        class="mb-1 block text-sm font-medium text-zinc-500 after:ml-0.5 after:text-red-500 after:content-['*'] dark:text-zinc-300">
-                        Documento
-                    </span>
-                    <div
-                        class="flex flex-col items-start justify-between rounded-xl border border-dashed border-zinc-400 p-2 dark:border-zinc-800">
-                        <div class="flex w-full justify-between">
-                            <div class="flex items-center gap-2">
-                                <label
-                                    class="flex w-max cursor-pointer items-center justify-center gap-1 text-nowrap rounded-xl border border-zinc-400 bg-white px-4 py-2 text-xs text-zinc-600 transition-colors duration-300 hover:bg-zinc-200/50 disabled:cursor-not-allowed disabled:bg-zinc-100/50 dark:border-zinc-800 dark:bg-zinc-950 dark:text-zinc-200 dark:hover:bg-zinc-900/50 sm:text-xs">
-                                    <input type="file" name="document" id="input-doc" class="input-doc hidden"
-                                        accept=".pdf" data-name="#file-name-project"
-                                        data-button-remove="#remove-file-project" />
-                                    <x-icon icon="file" class="h-4 w-4 text-zinc-600 dark:text-zinc-300" />
-                                    Adjuntar proyecto
-                                </label>
-                                <p id="file-name-project"
-                                    class="font-dine-r text-[10px] text-zinc-600 dark:text-zinc-300">
-                                    Formatos permitidos: .pdf
-                                </p>
-                            </div>
-                            <button class="remove-file hidden" id="remove-file-project" type="button"
-                                data-input="#input-doc">
-                                <x-icon icon="close" class="h-4 w-4 text-red-500" />
-                            </button>
-                        </div>
-                    </div>
-                    @error('document')
-                        <span class="mt-2 flex items-center gap-1 text-sm text-red-500">
-                            <x-icon icon="exclamation-circle" class="h-4 w-4" />
-                            <span class="error-msg text-red-500">
-                                {{ $message }}
-                            </span>
-                        </span>
-                    @enderror
+                    <x-input type="file" name="project" id="project" icon="file" required accept=".pdf, .docx"
+                        label="Documento" placeholder="Adjuntar proyecto" />
                 </div>
             </div>
             <div class="mt-6 flex items-center justify-center gap-4">
