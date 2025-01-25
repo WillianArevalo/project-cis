4d33cb8c923b6714fc8d0d1420f50c81dcef2358
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2025-01-24 21:30:03 -0600

    feat: Implement state change with AJAX on the state change button

--- resources/js/preguntas.js
+++ resources/js/preguntas.js
@@ -36,8 +36,21 @@ $(document).ready(function () {
         }
     });
 
-    $(".btn-add-note").on("click", function () {
-        const id = $(this).data("id");
-        $("#answer_id").val(id);
+    $(".btn-change-status").click(function () {
+        const url = $(this).data("action");
+        const status = $(this).data("value");
+
+        $.ajax({
+            url: url,
+            data: {
+                status: status,
+            },
+            success: function (response) {
+                location.reload();
+            },
+            error: function (error) {
+                console.log(error);
+            },
+        });
     });
 });
