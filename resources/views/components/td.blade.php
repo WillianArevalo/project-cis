061b9187c052f6c47927bd68c1f5628e8d7a28d0
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2025-03-15 12:41:14 -0600

    fix: Eliminar clase innecesaria en el componente td

--- resources/views/components/td.blade.php
+++ resources/views/components/td.blade.php
@@ -2,6 +2,6 @@
     'wrapper' => true,
 ])
 
-<td {{ $attributes->merge(['class' => $wrapper ? 'px-4 py-3 text-nowrap ' : 'px-4 py-3']) }}>
+<td {{ $attributes->merge(['class' => $wrapper ? 'px-4 py-3 ' : 'px-4 py-3']) }}>
     {{ $slot }}
 </td>
